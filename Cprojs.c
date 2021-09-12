db1261c92a28bbb8f4d86279e293a8851207aeb1
Featrz <81426878+Featrz@users.noreply.github.com>
2021-09-12 14:51:05 +0300

    Update Cprojs.c

--- Cprojs.c
+++ Cprojs.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 int main()
 {
@@ -7,18 +7,21 @@ int main()
 }
 int Viet() {
     int a, b, c;
-    printf("Input the following params:\n");
-    printf("a: "); scanf_s("%d", &a);
-    printf("b: "); scanf_s("%d", &b);
+    printf("Input the following params(a,b,c):\n");
+    printf("a: "); scanf_s("%d", &a); if (!a) { printf("No solutions"); return 0; }
+    printf("b: "); scanf_s("%d", &b); 
     printf("c: "); scanf_s("%d", &c);
     int x1 = min(-c / a, c / a);
     //printf("%d", -b / a);
     while (x1 < max(-c / a, c / a) + 1) {
         if((x1 != 0) && ((c / a) % x1 == 0) && (x1 + ((c / a)) / x1 == -b / a)) {
             printf("x1 = %d    x2 = %d", x1, ((c / a)) / x1);
+            return 0;
             break;
         }
         x1++;
     }
+    if (b * b - 4 * a * c < 0) { printf("No solutions"); return 0; }
+    printf("No whole solutions");
     return 0;
 }
